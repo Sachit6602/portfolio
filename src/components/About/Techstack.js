816353b87f20ecba7c8c4728ce87af8d5d8907b6
sachit6602 <freakysachit.sr@gmail.com>
2024-10-11 23:33:29 +0100

    update commit

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -5,7 +5,7 @@ import { TbPhotoEdit, TbAugmentedReality2 } from "react-icons/tb";
 import { AiOutlineVideoCameraAdd, AiFillAndroid } from "react-icons/ai";
 import { MdDesignServices } from "react-icons/md";
 import { IoLogoGameControllerB } from "react-icons/io";
-import {BsFiletypeSql} from "react-icons/bs";
+import { BsFiletypeSql } from "react-icons/bs";
 import { SiFlask } from "react-icons/si";
 import { SiNextdotjs } from "react-icons/si";
 import { GiArtificialIntelligence } from "react-icons/gi";
@@ -25,8 +25,6 @@ import {
   SiC,
   SiCsharp,
   SiCplusplus,
-  
-  
 } from "react-icons/si";
 import { ReactComponent as CV } from '../../Icons/cv.svg';
 import { ReactComponent as ML } from '../../Icons/ml.svg';
@@ -34,75 +32,75 @@ import { ReactComponent as ML } from '../../Icons/ml.svg';
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-    <Col xs={4} md={2} className="tech-icons">
-      <SiC />
-      <span className="tech-text">C Language</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <SiCplusplus />
-      <span className="tech-text">C++</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <SiCsharp />
-      <span className="tech-text">C#</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <DiPython />
-      <span className="tech-text">Python</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <DiJavascript1 />
-      <span className="tech-text">JavaScript</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <DiJava />
-      <span className="tech-text">Java</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <DiHtml5 />
-      <span className="tech-text">HTML5</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <GiArtificialIntelligence />
-      <span className="tech-text">AI</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <ML />
-      <span className="tech-text">Machine Learning</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <CV />
-      <span className="tech-text">Computer Vision</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <SiHiveBlockchain />
-      <span className="tech-text">Blockchain</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <SiFlask />
-      <span className="tech-text">Flask</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <BsFiletypeSql />
-      <span className="tech-text">SQL</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <AiFillAndroid />
-      <span className="tech-text">Android</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <TbAugmentedReality2 />
-      <span className="tech-text">Augmented Reality</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <DiReact />
-      <span className="tech-text">React</span>
-    </Col>
-    <Col xs={4} md={2} className="tech-icons">
-      <SiNextdotjs />
-      <span className="tech-text">Next.js</span>
-    </Col>
-  </Row>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiC />
+        <div className="tech-text">C Language</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCplusplus />
+        <div className="tech-text">C++</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCsharp />
+        <div className="tech-text">C#</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+        <div className="tech-text">Python</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+        <div className="tech-text">JavaScript</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+        <div className="tech-text">Java</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+        <div className="tech-text">HTML5</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <GiArtificialIntelligence />
+        <div className="tech-text">AI</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <ML />
+        <div className="tech-text">Machine Learning</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CV />
+        <div className="tech-text">Computer Vision</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHiveBlockchain />
+        <div className="tech-text">Blockchain</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlask />
+        <div className="tech-text">Flask</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <BsFiletypeSql />
+        <div className="tech-text">SQL</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <AiFillAndroid />
+        <div className="tech-text">Android</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <TbAugmentedReality2 />
+        <div className="tech-text">Augmented Reality</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+        <div className="tech-text">React</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+        <div className="tech-text">Next.js</div>
+      </Col>
+    </Row>
   );
 }
 
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -29,48 +29,63 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <ROS />
+        <div className="tech-text">ROS</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <Gazebo />
+        <div className="tech-text">Gazebo</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <MatLab />
+        <div className="tech-text">MATLAB</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiArduino />
+        <div className="tech-text">Arduino</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGooglecolab />
+        <div className="tech-text">Google Colab</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPycharm />
+        <div className="tech-text">PyCharm</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJupyter />
+        <div className="tech-text">Jupyter</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-      <SiVisualstudiocode />
+        <SiVisualstudiocode />
+        <div className="tech-text">VS Code</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAndroidstudio />
+        <div className="tech-text">Android Studio</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiUnity />
+        <div className="tech-text">Unity</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-      <SiLinux />
+        <SiLinux />
+        <div className="tech-text">Linux</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiBlender />
+        <div className="tech-text">Blender</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAdobephotoshop />
+        <div className="tech-text">Photoshop</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAdobepremierepro />
+        <div className="tech-text">Premiere Pro</div>
       </Col>
-     <Col xs={4} md={2} className="tech-icons">
-     <TfiMicrosoftAlt />
+      <Col xs={4} md={2} className="tech-icons">
+        <TfiMicrosoftAlt />
+        <div className="tech-text">Microsoft</div>
       </Col>
     </Row>
   );
